145b629a7c02a6fdef7547edd374be17f2bb79b6
Laura <lauraporto596@gmail.com>
2021-05-13 11:46:04 +0200

    articlecontroller created

--- app/Controllers/userController.js
+++ app/Controllers/userController.js
@@ -11,15 +11,15 @@ class UserController {
    return User.create(user);
   };
 
-  async signIn(user) { 
-    const errorMessage = { error_message: 'Wrong email or password' }
-    const user_list = await User.find(user)
+//   async signIn(user) { 
+//     const errorMessage = { error_message: 'Wrong email or password' }
+//     const user_list = await User.find(user)
 
-    if (user_list.length < 1) {
-      return errorMessage
-    }
-    return {user: user_list}
-  }
+//     if (user_list.length < 1) {
+//       return errorMessage
+//     }
+//     return {user: user_list}
+//   }
 
   async login(email,password) {
     const user =  await User.findOne({email});
@@ -45,18 +45,18 @@ class UserController {
   };
 
   async delete(id) {
-    return User.findByIdAndRemove(id)
+    return User.findByIdAndRemove(id);
   }
 
   async deleteAll(deleteUser) {
-    return User.deleteMany(deleteUser)
+    return User.deleteMany(deleteUser);
   }
 
   async userAll(allUser) {
-    return User.find(allUser)
+    return User.find(allUser);
   }
 
-}
+};
 
-const userController = new UserController
-module.exports = userController
+const userController = new UserController;
+module.exports = userController;
