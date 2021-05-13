4024f3cada6201dd9ab0c46d24f741afb1ba1aab
Laura <lauraporto596@gmail.com>
2021-05-13 11:25:57 +0200

    user and article models, routes, controllers

---
+++ app/router.js
@@ -0,0 +1,11 @@
+// const router = require('express').Router();
+
+// const userRouter = require('./Routes/userRouter');
+// const storeRouter = require('./Routes/storeRouter');
+
+
+// router.use('/user', userRouter);
+// router.use('/store', storeRouter);
+
+
+// module.exports = router;
